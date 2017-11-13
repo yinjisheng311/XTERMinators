132c12c7ef22bb2dcefa998a1fa538f243b01345
pappar1027 <zou_yun@mymail.sutd.edu.sg>
2017-11-13 22:36:43 +0800

    redirect page based on click event

--- webtraffic/views.py
+++ webtraffic/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home_view(request):
 	template_name='home.html'
 	return render(request,template_name)
+
+def host_view(request,host_name):
+	template_name='host.html'
+	context={'host':host_name}
+	return render(request,template_name,context)
